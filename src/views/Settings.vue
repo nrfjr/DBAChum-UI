8ea712e9050a9a144a8a5532a6342f807c1adce2
nrfjr <90370309+nrfjr@users.noreply.github.com>
2023-12-14 13:19:50 +0800

    simplified tab feature in Settings.vue

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,85 +6,50 @@
         <div class="tab-row-buttons w-[15%]">
             <ul class="flex-col text-sm font-medium justify-center" id="myTab" data-tabs-toggle="#myTabContent"
                 role="tablist">
-                <li class="mr-2">
+                <li v-for="([k, v]) in Object.entries(this.forms)" class="mr-2">
                     <div class="flex justify-start">
-                        <button @click="currentTab(1)" :class="tab === 1 ? 'font-bold text-white' : 'text-gray-400'"
-                            class="inline-block p-4 text-left" type="button">Connections</button>
-                    </div>
-                </li>
-                <li class="mr-2">
-                    <div class="flex justify-start">
-                        <button @click="currentTab(2)" :class="tab === 2 ? 'font-bold text-white' : 'text-gray-400'"
-                            class="inline-block p-4" type="button">Modules</button>
-                    </div>
-                </li>
-                <li class="mr-2 ">
-                    <div class="flex justify-start">
-                        <button @click="currentTab(3)" :class="tab === 3 ? 'font-bold text-white' : 'text-gray-400'"
-                            class="inline-block p-4" type="button">Disk
-                            Servers</button>
-                    </div>
-                </li>
-                <li class="mr-2">
-                    <div class="flex justify-start">
-                        <button @click="currentTab(4)" :class="tab === 4 ? 'font-bold text-white' : 'text-gray-400'"
-                            class="inline-block p-4 text-left" type="button">Preferences</button>
-                    </div>
-                </li>
-                <li class="mr-2">
-                    <div class="flex justify-start">
-                        <button @click="currentTab(5)" :class="tab === 5 ? 'font-bold text-white' : 'text-gray-400'"
-                            class="inline-block p-4 text-left" type="button">Advance</button>
+                        <button @click="currentTabHandler(k)" :class="tab === v ? 'font-bold text-white' : 'text-gray-400'"
+                            class="inline-block p-4 text-left" type="button">{{ k.replace("_", " ") }}</button>
                     </div>
                 </li>
             </ul>
         </div>
         <div id="myTabContent" class="ml-4 flex justify-center w-full scroll-y">
-            <div v-if="tab === 1" :class="tab === 1 ? '' : 'hidden'">
-                <Connections />
-            </div>
-            <div v-else-if="tab === 2" :class="tab === 2 ? '' : 'hidden'" class="w-[40%]">
-                <Modules />
-            </div>
-            <div v-else-if="tab === 3" :class="tab === 3 ? '' : 'hidden'">
-                <Disk_Servers />
-            </div>
-            <div v-else-if="tab === 4" :class="tab === 4 ? '' : 'hidden'" class="w-[40%] flex justify-center">
-                <Preferences />
-            </div>
-            <div v-else-if="tab === 5" :class="tab === 5 ? '' : 'hidden'" class="w-[40%]">
-                <AdvanceSettings />
-            </div>
+            <component :is="activeForm"/>
         </div>
     </div>
 </template>
 
 <script>
-import Connections from '../views/forms/Connections.vue'
-import AdvanceSettings from '../views/forms/AdvanceSettings.vue'
-import Disk_Servers from '../views/forms/Disk_Servers.vue'
-import Preferences from '../views/forms/Preferences.vue';
-import Modules from '../views/forms/Modules.vue';
+import { watchEffect } from 'vue';
 
 export default {
     data() {
         return {
             tab: 1,
+            activeForm: null,
+            forms:{
+                Connections: 1,
+                Modules: 2,
+                Disk_Servers: 3,
+                Preferences: 4,
+                Advance_Settings: 5
+            }
         };
     },
-    components:
-    {
-        Connections,
-        AdvanceSettings,
-        Disk_Servers,
-        Preferences,
-        Modules
-    }
-    ,
     methods: {
-        currentTab: function (tabNumber) {
-            this.tab = tabNumber;
+        currentTabHandler: function (tabName) {
+            this.tab = this.forms[tabName];
+            watchEffect(
+                        async () =>{ 
+                            import('/src/views/forms/'.concat(tabName,'.vue'))
+                            .then((result) => this.activeForm = result.default) 
+                        }
+            )    
         },
     },
+    mounted: function (){
+        this.currentTabHandler('Connections');
+    }
 };
 </script>
