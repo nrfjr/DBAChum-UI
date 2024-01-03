766dfef76ac41e210f815f73885b2160bac9039b
nrfjr <nurfajarsali@gmail.com>
2024-01-03 16:50:46 +0800

    Added doughnut chart component

---
+++ src/components/Doughnut.vue
@@ -0,0 +1,21 @@
+<template>
+  <DoughnutChart :chartData="this.chartValues" :options="this.chartOptions"/>
+</template>
+
+<script>
+import { DoughnutChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
+export default{
+  components:{
+    DoughnutChart
+  },
+  props:{
+    chartId: null,
+    chartValues: null,
+    chartOptions: null
+  }
+}
+</script>
